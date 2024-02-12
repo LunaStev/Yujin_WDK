5dcc219c4d8081cd22edf94fe3f07bea7bfb5765
Quin0019 <youngjae681@gmail.com>
2024-02-12 18:24:45 +0900

    After writing the part of the really, really essential lexer that's killing me, I'll sleep very, very comfortably. Good luck

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -2,15 +2,37 @@
  - lexer.rs
  */
 
+/**
+ - 토큰
+ */
 #[derive(Debug, PartialEq)]
 pub enum Token {
-    INT(i32), // int
-    VAR, // 변수
-    COUNT, // 숫자 변수
-    FUN, // 함수
-    PLUS, // 플러스
-    MINUS, // 마이너스
-    STAR, // 별
-    SLASH, // 슬래시
-    IMPORT, // 임포트
+    /** int */
+    INT(i32),
+
+    /** 변수 */
+    VAR,
+    /** 숫자 변수 */
+    COUNT,
+    /** 함수 */
+    FUN,
+    /** 플러스 */
+    PLUS,
+    /** 마이너스 */
+    MINUS,
+    /** 별 */
+    STAR,
+    /** 슬래시 */
+    SLASH,
+    /** 임포트 */
+    IMPORT,
+
+    /** 세미콜론 */
+    SEMICOLON,
+    /** 콤마 */
+    COMMA,
+
+    /** if */
+    IF,
+
 }
